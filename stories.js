e3223558187f6692f36e719bce834d3444ad2cd6
materDei3 <josephd26f@gmail.com>
2026-05-24 19:39:50 +0530

    Update stories.js

--- stories.js
+++ stories.js
@@ -3,21 +3,21 @@ const stories = [
 {
   image: "images/story1.jpg",
   title: "Reading",
-  text: "Late night Augustine reading.",
+  text: "wt do i put in this whole section.",
   date: "May 2026"
 },
 
 {
   image: "images/story2.jpg",
-  title: "Coffee",
-  text: "Coffee carrying theology again.",
+  title: "texts",
+  text: "it's under maintenance.",
   date: "May 2026"
 },
 
 {
   image: "images/story3.jpg",
   title: "Notes",
-  text: "Writing notes on memory.",
+  text: "yea didn't think of anything to write here.",
   date: "May 2026"
 }
 
