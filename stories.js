c5d221c33d5a17e069441423d0ba0505dc1ece72
materDei3 <josephd26f@gmail.com>
2026-05-24 19:01:28 +0530

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,24 @@
+const stories = [
+
+{
+  image: "images/story1.jpg",
+  title: "Reading",
+  text: "Late night Augustine reading.",
+  date: "May 2026"
+},
+
+{
+  image: "images/story2.jpg",
+  title: "Coffee",
+  text: "Coffee carrying theology again.",
+  date: "May 2026"
+},
+
+{
+  image: "images/story3.jpg",
+  title: "Notes",
+  text: "Writing notes on memory.",
+  date: "May 2026"
+}
+
+];
---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "my-site-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/essay1.html",
+  "/style.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
